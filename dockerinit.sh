0c51a9bbcc15771b917bdf587b4748663b9eba5d
Mateusz Żółtak <zozlak@zozlak.org>
2024-03-25 14:06:59 +0100

    dockerinit.sh: allow running metadata-crawler scripts

--- dockerinit.sh
+++ dockerinit.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 if [ -z "$DAEMONIZE" ] ; then
-    echo "### Performing virus scan" &&\
-    clamscan --recursive --infected /data &&\
-    echo "### Running the filechecker" &&\
-    /opt/vendor/bin/arche-filechecker -- /data /reports "$@" &&\
-    echo "### Ended successfully"
+    # https://stackoverflow.com/questions/1215538/extract-parameters-before-last-parameter-in
+    if [ "$1" == "crawl" ] ; then
+        php -f /opt/vendor/bin/arche-crawl-metadata -- "${@:2}" /mnt
+    elif [ "$1" == "createTemplate" ] ; then
+        php -f /opt/vendor/bin/arche-create-metadata-template -- /mnt "${@:2}"
+    elif [ "$1" == "check" ] ; then
+        php -f /opt/vendor/bin/arche-check-metadata -- "${@:2}"
+    else
+        echo "### Performing virus scan" &&\
+        clamscan --recursive --infected /data &&\
+        echo "### Running the filechecker" &&\
+        /opt/vendor/bin/arche-filechecker -- /data /reports "$@" &&\
+        echo "### Ended successfully"
+    fi
 else
     echo "### Running antivirus in a daemon mode"
     echo "###"
